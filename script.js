24d1f58f473e493f11dc5aa1c7fe06250721df3e
Andreas Hultman <hultman@outlook.com>
2021-03-12 22:13:47 +0100

    Functions added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
+function compute() {
+    p = 0; r = 0; y = 0;
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    document.getElementById("result").innerText = (calc(p, r, y));
 }
-        
+
+function calc(p, r, y) {
+    sum = Number(p);
+    for (i = 0; i < y; i++) {
+        sum = sum + (sum * (r / 100));
+        console.log(sum);
+    }
+
+    return sum;
+}
