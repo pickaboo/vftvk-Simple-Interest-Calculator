cc0e15dc7f9fb8855535c3a33c83eaebea799242
Andreas Hultman <hultman@outlook.com>
2021-03-13 01:26:52 +0100

    refix

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function compute() {
+var tmpr = document.getElementById("rate");
+var output = document.getElementById("rateout");
 
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    sum = calc(p, r, y);
-    document.getElementById("result").innerText = sum;
+output.innerHTML = tmpr.value;
+
+tmpr.oninput = function () {
+    output.innerHTML = this.value;
 }
+
+function compute() {
+    p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    sum = calc(p, r, y).toFixed(2);
+    var yr = (2020 + Number(y));
+
+    document.getElementById("am").innerHTML = "If you deposit <mark>" + p + "</mark>";
+    document.getElementById("ir").innerHTML = "at an interest rate of <mark> " + r + ' </mark>%';
+    document.getElementById("sm").innerHTML = "You will receive an amount of <mark>" + sum + "</mark>";
+    document.getElementById("iy").innerHTML = "in the year <mark>" + yr + "</mark>";
+
+}
+
 
 function calc(p, r, y) {
     sum = Number(p);
@@ -13,6 +28,5 @@ function calc(p, r, y) {
         sum = sum + (sum * (r / 100));
         console.log(sum);
     }
-
     return sum;
 }
