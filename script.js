fa966aeea62dfcf0da80f2e4bed27eb8e650a66c
Andreas Hultman <hultman@outlook.com>
2021-03-13 01:51:56 +0100

    last

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function compute() {
     p = document.getElementById("principal").value;
     var r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
-    sum = calc(p, r, y).toFixed(2);
-    var yr = (2020 + Number(y));
+    sum = calc(p, r, y).toFixed(2) - p;
+    var yr = (2021 + Number(y));
 
     document.getElementById("am").innerHTML = "If you deposit <mark>" + p + "</mark>";
     document.getElementById("ir").innerHTML = "at an interest rate of <mark> " + r + ' </mark>%';
-    document.getElementById("sm").innerHTML = "You will receive an amount of <mark>" + sum + "</mark>";
+    document.getElementById("sm").innerHTML = "You will receive an amount of <mark>" + sum.toFixed(2) + "</mark>";
     document.getElementById("iy").innerHTML = "in the year <mark>" + yr + "</mark>";
 
 }
